eba8c98d9104d4e6421ff92ad76b47fa9778390f
KX10Anonymous <kx10anonymous@gmail.com>
2023-11-09 11:16:30 +0200

    Added Web

---
+++ api/kioda/src/main/java/com/janon/kioda/KiodaApplication.java
@@ -0,0 +1,13 @@
+package com.janon.kioda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiodaApplication.class, args);
+	}
+
+}
---
+++ api/kioda/src/test/java/com/janon/kioda/KiodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janon.kioda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
